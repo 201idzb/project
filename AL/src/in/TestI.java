3488571cdebb9b41f5a812b9e1b9ddabe1443e32
MateuszRatajczyk <ratajfzg@gmail.com>
2014-06-01 22:53:52 +0200

    Update TestI.java

--- AL/src/in/TestI.java
+++ AL/src/in/TestI.java
@@ -1,4 +1,5 @@
 package in;
+package in;
 
 import java.sql.Timestamp;
 
@@ -9,13 +10,13 @@ import logmanager.Event.LogLevel;
 
 public class TestI extends Thread implements InputAdapter 
 {
-	@SuppressWarnings("unused")
-	private Configuration config;
-	private QueueManager queue;
+    @SuppressWarnings("unused")
+    private Configuration config;
+    private QueueManager queue;
 	
-	public TestI() { System.out.println("Utworzono Testowy Adapter Wejúciowy"); }
-	public void setupConfig(Configuration config) { this.config=config; }
-	public void connectToQueueManager(QueueManager queue) { this.queue=queue; }
+    public TestI() { System.out.println("Utworzono Testowy Adapter Wej≈õciowy"); }
+    public void setupConfig(Configuration config) { this.config=config; }
+    public void connectToQueueManager(QueueManager queue) { this.queue=queue; }
     public String test(String tmp) { return tmp; }
     public void exec() { start(); }
     
@@ -23,8 +24,8 @@ public class TestI extends Thread implements InputAdapter
     {
     	for (int i = 0; i < 110; i++) 
     	{
-    		Timestamp tmp = null;
-    		Event a=new Event(tmp, "yolo", LogLevel.INFO);
+    		//Timestamp tmp = null;
+    		Event a=new Event("timestamp", "logLevel", "details");
     		
     		System.out.println((queue.acceptEvent(a))?"Dodano Event":"Nie udalo sie dodac Eventu");
 		}
