9782d1ed3b6a135d8d1e7b0e5867200992005ead
MateuszRatajczyk <ratajfzg@gmail.com>
2014-06-01 22:12:10 +0200

    Update Event.java

--- AL/src/logmanager/Event.java
+++ AL/src/logmanager/Event.java
@@ -1,29 +1,38 @@
 package logmanager;
 
+import java.util.Date;
 import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 
 //przyklad utworzenia eventu
 //Timestamp tmp = null;
 //Event a=new Event(tmp, "yolo", LogLevel.INFO);
 
-
-
 public class Event 
 {	
 	public enum LogLevel { INFO, WARNING, ERROR, SEVERE };
 	private Timestamp timestamp;
-	private LogLevel loglevel;
+	private LogLevel logLevel;
 	private String details;
+	private DateFormat df = new SimpleDateFormat ("yyyy-MM-dd'T'HH:mm:ss.SSS");
+	private Date date;
+	
 	
 	public Timestamp getTimestamp() { return timestamp; }
 	public String getDetails()      { return details; }
-	public LogLevel getLoglevel()   { return loglevel; }
-
-	public Event(Timestamp timestamp, String details, LogLevel loglevel)
+	public LogLevel getLoglevel()   { return logLevel; }
+	
+	public Event(String timestamp, String logLevel, String details)
 	{
-		this.timestamp=timestamp;
-		this.details=details;
-		this.loglevel=loglevel;
+		try {
+			date = (Date)df.parse(timestamp);
+		} catch (ParseException e) {e.printStackTrace();}
+		
+		this.timestamp = new Timestamp(date.getTime());
+		this.details = details;
+		this.logLevel = LogLevel.valueOf(logLevel);
 	}
 }
