a50b5db5541526a0cf43025f054b54abf66a21c2
MateuszRatajczyk <ratajfzg@gmail.com>
2014-06-10 14:15:10 +0200

    Poprawione bledy (checkstyle)

--- AL/src/out/FileOutputAdapter.java
+++ AL/src/out/FileOutputAdapter.java
@@ -17,37 +17,37 @@ import logmanager.Event;
  *
  */
 public class FileOutputAdapter extends Thread implements OutputAdapter {
-	/**  Obiekt do klasy Configuration.  */
+    /**  Obiekt do klasy Configuration.  */
     private Configuration config;
     /**  Zmienna do obslugi zapisywania danych do pliku.  */
     private Writer out;
     /**  Zmienna do przechowywania linii z loga.  */
     private String tosave = "";
-    
+
     /**  Konstruktor wypisujacy utworzenie adaptera.  */
-    public FileOutputAdapter() { 
-    	System.out.println("Utworzono Adapter Wyjsciowy");
+    public FileOutputAdapter() {
+        System.out.println("Utworzono Adapter Wyjsciowy");
     }
-    
+
     /**
      * Metoda sluzaca do polaczenia z obiektem
      * konfiguracji za pomoca referencji z rdzenia aplikacji.
      * @param cnfg obiekt zawierajacy konfiguracje
      */
-    public final void setupConfig(final Configuration cnfg) { 
-    	this.config = cnfg;
+    public final void setupConfig(final Configuration cnfg) {
+        this.config = cnfg;
     }
-    
+
     /**
      * do obslugi funkcji testowej.
      * @param tmp test
      * @return null
      */
     public final String test(final String tmp) {
-    	return tmp;
+        return tmp;
     }
-    
-	 /**
+
+     /**
      * funkcja wywolania.
      */
     public final void exec() { start(); }
@@ -56,48 +56,48 @@ public class FileOutputAdapter extends Thread implements OutputAdapter {
      * metoda obslugi watku.
      */
     public final void run() {
-    	final int timeToSleep = 500;
-    	//utrzymywanie watku przy zyciu
-    	while (true) {
-    		try { 
-    			Thread.sleep(timeToSleep); 
-    		} catch (InterruptedException ex) { 
-    			Thread.currentThread().interrupt(); 
-    		}
-    	}  
+        final int timeToSleep = 500;
+        //utrzymywanie watku przy zyciu
+        while (true) {
+            try {
+                Thread.sleep(timeToSleep);
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
-    
+
     /**
      * metoda do przechwytywania Eventow.
-     * @param batch 
-     * @return true 
+     * @param batch paczka zdarzen
+     * @return true
      */
-	public final boolean storeEvents(final List<Event> batch) { 
-		//System.out.println("FileOutputAdapter - Przechwycono zdarzenia.");
-		for (@SuppressWarnings("rawtypes") Iterator iterator = batch.iterator();
-		iterator.hasNext();) {
-			Event event = (Event) iterator.next();
-			
-			//dopisywanie zdarzen do string`a + na koncu linii znak [Enter]
-			tosave = tosave + "{Timestamp: \"" + event.getTimestamp() 
-					+ "\", Loglevel: \"" + event.getLoglevel() 
-					+ "\", Details: \"" + event.getDetails() 
-					+ "\"}" + System.getProperty("line.separator");
-		}
-		try {
-			//System.out.println("Zapisuje do pliku...");
-			out = new BufferedWriter(new OutputStreamWriter(
-				  new FileOutputStream(config.getLocOutput())));
-			out.write(tosave);
-			out.flush();
-			out.close();
-		} catch (FileNotFoundException e) { 
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		//System.out.println("Zapisano do pliku!");
-		return true; //jesli ok
-	} 
+    public final boolean storeEvents(final List<Event> batch) {
+        //System.out.println("FileOutputAdapter - Przechwycono zdarzenia.");
+        for (@SuppressWarnings("rawtypes") Iterator iterator = batch.iterator();
+        iterator.hasNext();) {
+            Event event = (Event) iterator.next();
+
+            //dopisywanie zdarzen do string`a + na koncu linii znak [Enter]
+            tosave = tosave + "{Timestamp: \"" + event.getTimestamp()
+                    + "\", Loglevel: \"" + event.getLoglevel()
+                    + "\", Details: \"" + event.getDetails()
+                    + "\"}" + System.getProperty("line.separator");
+        }
+        try {
+            //System.out.println("Zapisuje do pliku...");
+            out = new BufferedWriter(new OutputStreamWriter(
+                  new FileOutputStream(config.getLocOutput())));
+            out.write(tosave);
+            out.flush();
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //System.out.println("Zapisano do pliku!");
+        return true; //jesli ok
+    }
 }
