13eaa3d69bfaf25591609ac2d31be65cfe955165
MateuszRatajczyk <ratajfzg@gmail.com>
2014-06-10 20:12:53 +0200

    Poprawiony zapis do formatu JSON

--- AL/src/out/FileOutputAdapter.java
+++ AL/src/out/FileOutputAdapter.java
@@ -17,133 +17,131 @@ import com.google.gson.GsonBuilder;
 import logmanager.Configuration;
 import logmanager.Event;
 import logmanager.Event.LogLevel;
+
 /**
- * wyjsciowy adapter zapisu do pliku.
- * @author Mateusz Ratajczyk
- *
- */
+* wyjsciowy adapter zapisu do pliku.
+* @author Mateusz Ratajczyk
+*
+*/
 public class FileOutputAdapter extends Thread implements OutputAdapter {
-    /**  Obiekt do klasy Configuration.  */
-    private Configuration config;
-    /**  Zmienna do obslugi zapisywania danych do pliku.  */
-    private Writer out;
-    /**  Zmienna do przechowywania linii z loga.  */
-    private String jsonRepresentation = "";
-    
-    /**  Obiekt do przechowywania szablonu zapisu do JSON.  */
-    private SomeObject someObject;
-    
-    /**  Konstruktor wypisujacy utworzenie adaptera.  */
-    public FileOutputAdapter() {
-        System.out.println("Utworzono Adapter Wyjsciowy");
-    }
-    
-    /** prywatna klasa, ktora posiada osobny konstruktor
-     * inicjalizujacy zmienne Timestamp, Loglevel, Details.
-     */
-    private class SomeObject {
-    	  /**  Zmienna do przechowywania Timestamp`a.  */
-    	  @SuppressWarnings("unused")
-		  private final Timestamp timestamp;
-    	  /**  Zmienna do przechowywania Loglevel`a.  */
-    	  @SuppressWarnings("unused")
-		  private final LogLevel loglevel;
-    	  /**  Zmienna do przechowywania Details`a.  */
-    	  @SuppressWarnings("unused")
-		  private final String details;
+	/**  Obiekt do klasy Configuration.  */
+	private Configuration config;
+	/**  Zmienna do obslugi zapisywania danych do pliku.  */
+	private Writer out;
+	/**  Zmienna do przechowywania linii z loga.  */
+	private String jsonRepresentation = "";
 
-    	  /** konstruktor do tworzenia szablonu
-    	   *  wykorzystywanego do zapisu do formatu JSON.
-    	   *  @param a Timestamp
-    	   *  @param b logLevel
-    	   *  @param c String
-    	   */
-    	  public SomeObject(final Timestamp a, 
-    			  final LogLevel b, final String c) {
-    	    this.timestamp = a;
-    	    this.loglevel = b;
-    	    this.details = c;
-    	  }
-    	}
-    
-    /**
-     * Metoda sluzaca do polaczenia z obiektem
-     * konfiguracji za pomoca referencji z rdzenia aplikacji.
-     * @param cnfg obiekt zawierajacy konfiguracje
-     */
-    public final void setupConfig(final Configuration cnfg) {
-        this.config = cnfg;
-    }
+	/**  Obiekt do przechowywania szablonu zapisu do JSON.  */
+	private SomeObject someObject;
 
-    /**
-     * do obslugi funkcji testowej.
-     * @param tmp test
-     * @return null
-     */
-    public final String test(final String tmp) {
-        return tmp;
-    }
+	/**  Konstruktor wypisujacy utworzenie adaptera.  */
+	public FileOutputAdapter() {
+		System.out.println("Utworzono Adapter Wyjsciowy");
+	}
 
-     /**
-     * funkcja wywolania.
-     */
-    public final void exec() { start(); }
+	/** prywatna klasa, ktora posiada osobny konstruktor
+	* inicjalizujacy zmienne Timestamp, Loglevel, Details.
+	*/
+	private class SomeObject {
+		/**  Zmienna do przechowywania Timestamp`a.  */
+		@SuppressWarnings("unused")
+		private final Timestamp timestamp;
+		/**  Zmienna do przechowywania Loglevel`a.  */
+		@SuppressWarnings("unused")
+		private final LogLevel loglevel;
+		/**  Zmienna do przechowywania Details`a.  */
+		@SuppressWarnings("unused")
+		private final String details;
 
-    /**
-     * metoda obslugi watku.
-     */
-    public final void run() {
-        final int timeToSleep = 500;
-        //utrzymywanie watku przy zyciu
-        while (true) {
-            try {
-                Thread.sleep(timeToSleep);
-            } catch (InterruptedException ex) {
-                Thread.currentThread().interrupt();
-            }
-        }
-    }
+		/** konstruktor do tworzenia szablonu
+		*  wykorzystywanego do zapisu do formatu JSON.
+		*  @param a Timestamp
+		*  @param b logLevel
+		*  @param c String
+		*/
+		public SomeObject(final Timestamp a, 
+		final LogLevel b, final String c) {
+			this.timestamp = a;
+			this.loglevel = b;
+			this.details = c;
+		}
+	}
 
-    /**
-     * metoda do przechwytywania Eventow.
-     * @param batch paczka zdarzen
-     * @return true
-     */
-    public final boolean storeEvents(final List<Event> batch) {
-        //System.out.println("FileOutputAdapter - Przechwycono zdarzenia.");
-        Gson gson = new GsonBuilder().setFieldNamingPolicy(
-        		FieldNamingPolicy.UPPER_CAMEL_CASE).create();
-        String tmpString = "";
+	/**
+	* Metoda sluzaca do polaczenia z obiektem
+	* konfiguracji za pomoca referencji z rdzenia aplikacji.
+	* @param cnfg obiekt zawierajacy konfiguracje
+	*/
+	public final void setupConfig(final Configuration cnfg) {
+		this.config = cnfg;
+	}
 
-        for (@SuppressWarnings("rawtypes") Iterator iterator = batch.iterator();
-        iterator.hasNext();) {
-            Event event = (Event) iterator.next();          
-            
-            someObject = new SomeObject(
-            		event.getTimestamp(),
-            		event.getLoglevel(),
-            		event.getDetails());
-            
-            tmpString = gson.toJson(someObject);
-            
-            jsonRepresentation = jsonRepresentation
-            		+ tmpString
-            		+ System.getProperty("line.separator");
-            
-            try {
-            	out = new BufferedWriter(new OutputStreamWriter(
-            			new FileOutputStream(config.getLocOutput())));
-            	out.write(jsonRepresentation);
-            	out.flush();
-            	out.close();
-        	
-            } catch (FileNotFoundException e) {
-            	e.printStackTrace();
-            } catch (IOException e) {
-            	e.printStackTrace();
-            }
-        //System.out.println("Zapisano do pliku!");
-        }
-        return true; //jesli ok
-    }
+	/**
+	* do obslugi funkcji testowej.
+	* @param tmp test
+	* @return null
+	*/
+	public final String test(final String tmp) { return tmp; }
+
+	/**
+	* funkcja wywolania.
+	*/
+	public final void exec() { start(); }
+
+	/**
+	* metoda do przechwytywania Eventow.
+	* @param batch paczka zdarzen
+	* @return true
+	*/
+	public final boolean storeEvents(final List<Event> batch) {
+		//System.out.println("FileOutputAdapter - Przechwycono zdarzenia.");
+		Gson gson = new GsonBuilder().setFieldNamingPolicy(
+		FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+		String tmpString = "";
+
+		for (@SuppressWarnings("rawtypes") Iterator iterator = batch.iterator();
+		iterator.hasNext();) {
+			Event event = (Event) iterator.next();          
+
+			someObject = new SomeObject(
+			event.getTimestamp(),
+			event.getLoglevel(),
+			event.getDetails());
+
+			tmpString = gson.toJson(someObject);
+
+			jsonRepresentation = jsonRepresentation
+			+ tmpString
+			+ System.getProperty("line.separator");
+
+			try {
+				out = new BufferedWriter(new OutputStreamWriter(
+				new FileOutputStream(config.getLocOutput())));
+				out.write(jsonRepresentation);
+				out.flush();
+				out.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			//System.out.println("Zapisano do pliku!");
+		}
+		return true; //jesli ok
+	}
+
+	/**
+	* metoda obslugi watku.
+	*/
+	public final void run() {
+		final int timeToSleep = 500;
+		//utrzymywanie watku przy zyciu
+		while (true) {
+			try {
+				Thread.sleep(timeToSleep);
+			} catch (InterruptedException ex) {
+				Thread.currentThread().interrupt();
+			}
+		}
+	}
 }
